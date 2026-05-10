f0a20c987c74468603b8634279a2db3aea11c508
mafinsharo <mafinsharo444@gmail.com>
2026-05-10 14:33:06 +0530

    changes made by american guy

--- src/test/java/org/automation/testing/TC001_SignInAlertTest.java
+++ src/test/java/org/automation/testing/TC001_SignInAlertTest.java
@@ -40,6 +40,7 @@ public class TC001_SignInAlertTest extends BaseClass {
         System.out.println("✅ Test case TC001_SignInAlert passed - Test case is valid");
         System.out.println("Made changes in this code");
         System.out.println("TC01 is working fine");
+        System.out.println("Hello World");
 
     }
 }
