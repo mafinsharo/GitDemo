2f559e6bdffc6f9cd827d4afb033c9aea7c76441
mafinsharo <mafinsharo444@gmail.com>
2026-05-10 16:24:28 +0530

    feature added in develop branch

--- src/test/java/org/automation/testing/TC002_ForgotPasswordAlertTest.java
+++ src/test/java/org/automation/testing/TC002_ForgotPasswordAlertTest.java
@@ -40,7 +40,7 @@ public class TC002_ForgotPasswordAlertTest extends BaseClass {
         System.out.println("✅ Test case TC002_ForgotPasswordAlert passed - Test case is valid");
 
         driver.navigate().back();
-
+        System.out.println("changes made in tc002");
 
     }
 }
